b4483dd7ed0103a85f9a9ae86fff2c66a007127f
YSRKEN <ysr.ken@gmail.com>
2017-01-07 14:44:55 +0900

    バージョン情報を正しく表示できるようにした

--- WsGH/MainWindow.xaml.cs
+++ WsGH/MainWindow.xaml.cs
@@ -20,27 +20,46 @@ namespace WsGH {
 	/// </summary>
 	public partial class MainWindow : Window {
 		ScreenshotProvider sp;
-
+		// コンストラクタ
 		public MainWindow() {
 			InitializeComponent();
 		}
-
+		// メニュー操作
 		private void ExitMenu_Click(object sender, RoutedEventArgs e) {
 			Close();
 		}
-
 		private void GetPositionMenu_Click(object sender, RoutedEventArgs e) {
 			sp = new ScreenshotProvider(new AfterAction(GetPosition));
 		}
-
 		private void GetScreenshotMenu_Click(object sender, RoutedEventArgs e) {
 			saveScreenshot();
 		}
-
+		private void AboutMenu_Click(object sender, RoutedEventArgs e) {
+			// 自分自身のバージョン情報を取得する
+			// (http://dobon.net/vb/dotnet/file/myversioninfo.html)
+			// AssemblyTitle
+			var asmttl = ((System.Reflection.AssemblyTitleAttribute)
+				Attribute.GetCustomAttribute(
+				System.Reflection.Assembly.GetExecutingAssembly(),
+				typeof(System.Reflection.AssemblyTitleAttribute))).Title;
+			// AssemblyCopyright
+			var asmcpy = ((System.Reflection.AssemblyCopyrightAttribute)
+				Attribute.GetCustomAttribute(
+				System.Reflection.Assembly.GetExecutingAssembly(),
+				typeof(System.Reflection.AssemblyCopyrightAttribute))).Copyright;
+			// AssemblyProduct
+			var asmprd = ((System.Reflection.AssemblyProductAttribute)
+				Attribute.GetCustomAttribute(
+				System.Reflection.Assembly.GetExecutingAssembly(),
+				typeof(System.Reflection.AssemblyProductAttribute))).Product;
+			var asmver = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+			MessageBox.Show(asmttl + " Ver." + asmver + "\n" + asmcpy + "\n" + asmprd);
+		}
+		// ボタン操作
 		private void ScreenShotButton_Click(object sender, RoutedEventArgs e) {
 			saveScreenshot();
 		}
-
+		// 座標取得後の画面更新処理
 		void GetPosition() {
 			if(sp.isGetPosition()) {
 				GetScreenshotMenu.IsEnabled = ScreenShotButton.IsEnabled = true;
@@ -48,7 +67,7 @@ namespace WsGH {
 				GetScreenshotMenu.IsEnabled = ScreenShotButton.IsEnabled = false;
 			}
 		}
-
+		// 画像保存処理
 		void saveScreenshot() {
 			sp.getScreenShot().Save("test.png");
 		}
--- WsGH/Properties/AssemblyInfo.cs
+++ WsGH/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("WsGH")]
+[assembly: AssemblyTitle("WarshipGirls Helper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WsGH")]
+[assembly: AssemblyProduct("Product by YSR")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
