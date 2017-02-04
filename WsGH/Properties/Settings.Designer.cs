589341e4259746ca393561e218d59ffddadd3725
YSRKEN <ysr.ken@gmail.com>
2017-02-05 02:48:58 +0900

    タイマーの時間を設定で保存するようにした

--- WsGH/Properties/Settings.Designer.cs
+++ WsGH/Properties/Settings.Designer.cs
@@ -67,5 +67,197 @@ namespace WsGH.Properties {
                 this["ShowTimerWindowFlg"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong ExpTimer1 {
+            get {
+                return ((ulong)(this["ExpTimer1"]));
+            }
+            set {
+                this["ExpTimer1"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong ExpTimer2 {
+            get {
+                return ((ulong)(this["ExpTimer2"]));
+            }
+            set {
+                this["ExpTimer2"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong ExpTimer3 {
+            get {
+                return ((ulong)(this["ExpTimer3"]));
+            }
+            set {
+                this["ExpTimer3"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong ExpTimer4 {
+            get {
+                return ((ulong)(this["ExpTimer4"]));
+            }
+            set {
+                this["ExpTimer4"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong BldTimer1 {
+            get {
+                return ((ulong)(this["BldTimer1"]));
+            }
+            set {
+                this["BldTimer1"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong BldTimer2 {
+            get {
+                return ((ulong)(this["BldTimer2"]));
+            }
+            set {
+                this["BldTimer2"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong BldTimer3 {
+            get {
+                return ((ulong)(this["BldTimer3"]));
+            }
+            set {
+                this["BldTimer3"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong BldTimer4 {
+            get {
+                return ((ulong)(this["BldTimer4"]));
+            }
+            set {
+                this["BldTimer4"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DevTimer1 {
+            get {
+                return ((ulong)(this["DevTimer1"]));
+            }
+            set {
+                this["DevTimer1"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DevTimer2 {
+            get {
+                return ((ulong)(this["DevTimer2"]));
+            }
+            set {
+                this["DevTimer2"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DevTimer3 {
+            get {
+                return ((ulong)(this["DevTimer3"]));
+            }
+            set {
+                this["DevTimer3"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DevTimer4 {
+            get {
+                return ((ulong)(this["DevTimer4"]));
+            }
+            set {
+                this["DevTimer4"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DockTimer1 {
+            get {
+                return ((ulong)(this["DockTimer1"]));
+            }
+            set {
+                this["DockTimer1"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DockTimer2 {
+            get {
+                return ((ulong)(this["DockTimer2"]));
+            }
+            set {
+                this["DockTimer2"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DockTimer3 {
+            get {
+                return ((ulong)(this["DockTimer3"]));
+            }
+            set {
+                this["DockTimer3"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public ulong DockTimer4 {
+            get {
+                return ((ulong)(this["DockTimer4"]));
+            }
+            set {
+                this["DockTimer4"] = value;
+            }
+        }
     }
 }
