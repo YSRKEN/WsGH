e6a290df5df60cff4bd5d890d0c00e1d2ee65a7b
YSRKEN <ysr.ken@gmail.com>
2017-02-05 00:54:48 +0900

    シーン認識機能をぬるっと搭載。遠征画面かどうかを判断できるようになった

--- WsGH/SceneRecognition.cs
+++ WsGH/SceneRecognition.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing.Imaging;
 
 namespace WsGH {
 	static class SceneRecognition {
@@ -12,14 +13,58 @@ namespace WsGH {
 		/// 座標・大きさは画像に対する％指定なことに注意
 		/// </summary>
 		/// <param name="bitmap">画像</param>
-		/// <param name="px">切り取る左座標</param>
-		/// <param name="py">切り取る上座標</param>
-		/// <param name="wx">切り取る幅</param>
-		/// <param name="wy">切り取る高さ</param>
+		/// <param name="px_per">切り取る左座標</param>
+		/// <param name="py_per">切り取る上座標</param>
+		/// <param name="wx_per">切り取る幅</param>
+		/// <param name="wy_per">切り取る高さ</param>
 		/// <returns>64bitのDifferenceHash値</returns>
-		static ulong getDifferenceHash(Bitmap bitmap, double px, double py, double wx, double wy) {
-			//スタブ
-			return 0;
+		static ulong getDifferenceHash(Bitmap bitmap, double px_per, double py_per, double wx_per, double wy_per) {
+			// ％指定をピクセル指定に直す
+			var bitmapWidth = bitmap.Width;
+			var bitmapHeight = bitmap.Height;
+			var px = (int)(bitmapWidth * px_per / 100);
+			var py = (int)(bitmapHeight * py_per / 100);
+			var wx = (int)(bitmapWidth * wx_per / 100);
+			var wy = (int)(bitmapHeight * wy_per / 100);
+			// 画像を切り取り、横9ピクセル縦8ピクセルにリサイズする
+			// その際にグレースケール化も同時に施す
+			var canvas = new Bitmap(9, 8);
+			using(var g = Graphics.FromImage(canvas)) {
+				// 切り取られる位置・大きさ
+				var srcRect = new Rectangle(px, py, wx, wy);
+				// 貼り付ける位置・大きさ
+				var desRect = new Rectangle(0, 0, canvas.Width, canvas.Height);
+				// グレースケール変換用のマトリックスを設定
+				var cm = new ColorMatrix(
+					new float[][]{
+						new float[]{0.299f, 0.299f, 0.299f, 0 ,0},
+						new float[]{0.587f, 0.587f, 0.587f, 0, 0},
+						new float[]{0.114f, 0.114f, 0.114f, 0, 0},
+						new float[]{0, 0, 0, 1, 0},
+						new float[]{0, 0, 0, 0, 1}
+					}
+				);
+				var ia = new ImageAttributes();
+				ia.SetColorMatrix(cm);
+				// 描画
+				// ImageAttributesを設定しなければ、
+				// g.DrawImage(bitmap, desRect, srcRect, GraphicsUnit.Pixel);
+				// で済むのにMSェ……
+				g.DrawImage(
+					bitmap, desRect, srcRect.X, srcRect.Y, 
+					srcRect.Width, srcRect.Height, GraphicsUnit.Pixel, ia
+				);
+			}
+			// 隣接ピクセルとの比較結果を符号化する
+			ulong hash = 0;
+			for(int y = 0; y < 8; ++y) {
+				for(int x = 0; x < 8; ++x) {
+					hash <<= 1;
+					if(canvas.GetPixel(x, y).R > canvas.GetPixel(x + 1, y).R)
+						hash |= 1;
+				}
+			}
+			return hash;
 		}
 		// ビットカウント
 		static uint popcnt(ulong x) {
@@ -29,7 +74,7 @@ namespace WsGH {
 			x = ((x & 0xff00ff00ff00ff00) >> 8) + (x & 0x00ff00ff00ff00ff);
 			x = ((x & 0xffff0000ffff0000) >> 16) + (x & 0x0000ffff0000ffff);
 			x = ((x & 0xffffffff00000000) >> 32) + (x & 0x00000000ffffffff);
-			return x;
+			return (uint)x;
 		}
 		// ハミング距離を計算する
 		static uint getHummingDistance(ulong a, ulong b) {
@@ -39,12 +84,12 @@ namespace WsGH {
 		public static bool isExpeditionScene(Bitmap bitmap) {
 			{
 				var hash = getDifferenceHash(bitmap, 10.11, 76.36, 3.525, 6.276);
-				if(getHummingDistance(hash, 0x2d2a17a726222a2a) >= 20)
+				if(getHummingDistance(hash, 0x2d2e2ba5aaa22a2a) >= 20)
 					return false;
 			}
 			{
 				var hash = getDifferenceHash(bitmap, 21.86, 62.76, 3.878, 1.883);
-				if(getHummingDistance(hash, 0x94d072682d656476) >= 20)
+				if(getHummingDistance(hash, 0xd2d0b8a8a4545656) >= 20)
 					return false;
 			}
 			return true;
