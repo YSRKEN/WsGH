2d0762fee13103dc270e7312dbc7c58f330f9906
YSRKEN <ysr.ken@gmail.com>
2017-02-11 00:19:55 +0900

    多言語切り替えロジックを組み込む

---
+++ WsGH/ResourceService.cs
@@ -0,0 +1,55 @@
+﻿/* 多言語対応リソースファイルを任意で切り替えられるようにする
+ * http://grabacr.net/archives/1647
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WsGH {
+	public class ResourceService : INotifyPropertyChanged {
+		#region singleton members
+
+		private static readonly ResourceService _current = new ResourceService();
+		public static ResourceService Current {
+			get { return _current; }
+		}
+
+		#endregion
+
+		private readonly WsGH.Properties.Resources _resources = new Properties.Resources();
+
+		/// <summary>
+		/// 多言語化されたリソースを取得します。
+		/// </summary>
+		public WsGH.Properties.Resources Resources {
+			get { return this._resources; }
+		}
+
+		#region INotifyPropertyChanged members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+			var handler = this.PropertyChanged;
+			if(handler != null)
+				handler(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+
+		/// <summary>
+		/// 指定されたカルチャ名を使用して、リソースのカルチャを変更します。
+		/// </summary>
+		/// <param name="name">カルチャの名前。</param>
+		public void ChangeCulture(string name) {
+			Properties.Resources.Culture = CultureInfo.GetCultureInfo(name);
+			this.RaisePropertyChanged("Resources");
+		}
+	}
+}
